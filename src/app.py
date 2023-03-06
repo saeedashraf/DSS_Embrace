2e670917ef3abe66ef06edc92f44ee19d9a0c82c
Andrei V. Plamada <andrei.plamada@uzh.ch>
2023-03-06 07:35:45 +0100

    feat: integrate historical data via plotly

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
+import numpy as np
+import pandas as pd
 import panel as pn
 import param
+import plotly.graph_objects as go
 
 template = pn.template.MaterialTemplate(
     title="DSS_Embrace",
@@ -50,8 +53,93 @@ class DSS_Embrace(param.Parameterized):
 
     @param.depends("show_historical_data", "historical_data_range")
     def view_show_historical_data(self):
+        df_from_csv_dfq50_years_obs = pd.read_csv("./src/data/dfq50_years_obs.csv")
+        arr_from_sql_dfq50_years_obs = df_from_csv_dfq50_years_obs[
+            "q50_years_obs"
+        ].values
+
+        df_from_csv_dfq75_years_obs = pd.read_csv("./src/data/dfq75_years_obs.csv")
+        arr_from_sql_dfq75_years_obs = df_from_csv_dfq75_years_obs[
+            "q75_years_obs"
+        ].values
+
+        df_from_csv_dfq90_years_obs = pd.read_csv("./src/data/dfq90_years_obs.csv")
+        arr_from_sql_dfq90_years_obs = df_from_csv_dfq90_years_obs[
+            "q90_years_obs"
+        ].values
+
+        df_from_csv = pd.read_csv("./src/data/array_data_matrix_HotDays_obs.csv")
+        arr_from_sql_matrix_HotDays_obs = df_from_csv.values.reshape((80, 41, 2))
+
+        # TODO: hardcoded - it should come from the the previous arr
+        x_axis_obs = np.arange(1981, 2020 + 1)
+
+        p_Step = 1
+        alpha_Fade_obs = 1
+
+        fig = go.Figure()
+
+        for i in range(0, len(arr_from_sql_matrix_HotDays_obs), p_Step):
+            a = arr_from_sql_matrix_HotDays_obs[i, 1:, 1:2].astype(float)
+            fig.add_trace(
+                go.Scatter(
+                    x=x_axis_obs,
+                    y=a[:, 0],
+                    line=dict(color="green", dash="dash"),
+                    opacity=alpha_Fade_obs,
+                )
+            )
+
+        fig.add_trace(
+            go.Scatter(
+                x=x_axis_obs,
+                y=arr_from_sql_dfq50_years_obs.flatten(),
+                line=dict(color="black", width=2.5),
+                opacity=1,
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=x_axis_obs,
+                y=arr_from_sql_dfq75_years_obs.flatten(),
+                line=dict(color="yellow", width=2.5),
+                opacity=1,
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=x_axis_obs,
+                y=arr_from_sql_dfq90_years_obs.flatten(),
+                line=dict(color="red", width=2.5),
+                opacity=1,
+            )
+        )
+        # Andrei: remove `ticktext=values`, dynamig range using historical_data_range
+        # Customize x-axis
+        fig.update_xaxes(
+            title="Years",
+            tickvals=x_axis_obs,
+            tickfont=dict(size=15),
+            range=[self.historical_data_range[0], self.historical_data_range[1]],
+            showgrid=True,
+        )
+
+        # Customize y-axis
+        fig.update_yaxes(
+            title="Number of Hot Days & Nights",
+            titlefont=dict(size=17),
+            range=[0, 30],
+            showgrid=True,
+        )
+
+        # Add title to the figure
+        fig.update_layout(
+            title="Historical number of Hot Days & Nights (M1) Over the Years",
+            title_font=dict(size=20),
+        )
+
         if self.show_historical_data is True:
-            return pn.pane.PNG("./src/fig/1.png", width=800)
+            return fig
 
         else:
             return None
