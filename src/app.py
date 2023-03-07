3e54c1d61796a2b9071b116742e026bb2052f262
Andrei V. Plamada <andrei.plamada@uzh.ch>
2023-03-07 13:17:38 +0100

    add number_of_hot_days plots

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import panel as pn
@@ -41,13 +43,113 @@ class DSS_Embrace(param.Parameterized):
         True, label="Show Feature Scoring", precedence=3
     )
 
+    def _plot_number_of_hot_days_and_nights(self, climate_scenario):
+        parent_directory = Path("./src/data/")
+        scenario_as_number = climate_scenario[-3] + climate_scenario[-1]
+        color_dict = {"26": "green", "45": "orange", "85": "red"}
+
+        f"./src/data/dfq90_years_{scenario_as_number}.csv",
+        f"q90_years_{scenario_as_number}",
+        f"./src/data/array_data_matrix_HotDays_{scenario_as_number}.csv",
+
+        column = f"q50_years_{scenario_as_number}"
+        df_from_csv_dfq50_years = pd.read_csv(parent_directory / f"df{column}.csv")
+        arr_from_sql_dfq50_years = df_from_csv_dfq50_years[column].values
+
+        column = f"q75_years_{scenario_as_number}"
+        df_from_csv_dfq75_years = pd.read_csv(parent_directory / f"df{column}.csv")
+        arr_from_sql_dfq75_years = df_from_csv_dfq75_years[column].values
+
+        column = f"q90_years_{scenario_as_number}"
+        df_from_csv_dfq90_years = pd.read_csv(parent_directory / f"df{column}.csv")
+        arr_from_sql_dfq90_years = df_from_csv_dfq90_years[column].values
+
+        df_from_csv = pd.read_csv(
+            parent_directory / f"array_data_matrix_HotDays_{scenario_as_number}.csv"
+        )
+        arr_from_sql_matrix_HotDays = df_from_csv.values.reshape((5440, 120, 2))
+
+        p_Step = 20
+        alpha_Fade = 1
+
+        fig = go.Figure()
+
+        x_axis = np.arange(2020, 2101, step=1)
+
+        for i in range(0, len(arr_from_sql_matrix_HotDays), p_Step):
+            a = arr_from_sql_matrix_HotDays[i, 1:, 1:2].astype(float)
+            fig.add_trace(
+                go.Scatter(
+                    x=x_axis,
+                    y=a[:, 0],
+                    mode="lines",
+                    line=dict(color=color_dict[scenario_as_number], dash="dash"),
+                    opacity=alpha_Fade,
+                )
+            )
+
+        fig.add_trace(
+            go.Scatter(
+                x=x_axis,
+                y=arr_from_sql_dfq50_years[:],
+                mode="lines",
+                line=dict(color="black", width=1.5),
+                opacity=1,
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=x_axis,
+                y=arr_from_sql_dfq75_years[:],
+                mode="lines",
+                line=dict(color="yellow", width=1.5),
+                opacity=1,
+            )
+        )
+        fig.add_trace(
+            go.Scatter(
+                x=x_axis,
+                y=arr_from_sql_dfq90_years[:],
+                mode="lines",
+                line=dict(color="red", width=2),
+                opacity=1,
+            )
+        )
+        # Andrei: range based on scenarios_data_range, remove tickvals
+        # Customize x-axis
+        fig.update_xaxes(
+            range=[self.scenarios_data_range[0], self.scenarios_data_range[1]],
+            tickfont=dict(size=20),
+        )
+
+        # Customize y-axis
+        fig.update_yaxes(
+            title="Number of Hot Days & Nights (M1)",
+            titlefont=dict(size=20),
+            range=[0, 110],
+            tickfont=dict(size=20),
+        )
+
+        # Set subplot title
+        fig.update_layout(
+            title=f"Number of Hot Days & Nights (M1) Over the Years in Zürich for {climate_scenario} (2020-2100)",
+            title_font=dict(size=20),
+            xaxis_title="Year",
+            yaxis_title="Number of Hot Days & Nights",
+            width=600,
+        )
+
+        return fig
+
     @param.depends("climate_scenarios", "scenarios_data_range")
     def view(self):
         res = pn.Column()
         for el in self.climate_scenarios:
-            row = pn.Row()
-            for fig in self.figs_climate_scenarios[el]:
-                row.append(pn.pane.PNG(f"./src/fig/{fig}", width=600))
+
+            new_fig = self._plot_number_of_hot_days_and_nights(el)
+            fig = self.figs_climate_scenarios[el][1]
+            row = pn.Row(new_fig, pn.pane.PNG(f"./src/fig/{fig}", width=600))
+
             res.append(row)
         return res
 
@@ -114,12 +216,12 @@ class DSS_Embrace(param.Parameterized):
                 opacity=1,
             )
         )
-        # Andrei: remove `ticktext=values`, dynamig range using historical_data_range
+        # Andrei: remove `ticktext=values`, ` tickvals=x_axis_obs,` dynamig range using historical_data_range
+        # Andrei: use fonsize 20 as in the previous charts
         # Customize x-axis
         fig.update_xaxes(
             title="Years",
-            tickvals=x_axis_obs,
-            tickfont=dict(size=15),
+            tickfont=dict(size=20),
             range=[self.historical_data_range[0], self.historical_data_range[1]],
             showgrid=True,
         )
@@ -127,19 +229,18 @@ class DSS_Embrace(param.Parameterized):
         # Customize y-axis
         fig.update_yaxes(
             title="Number of Hot Days & Nights",
-            titlefont=dict(size=17),
+            titlefont=dict(size=20),
             range=[0, 30],
             showgrid=True,
         )
 
         # Add title to the figure
+        # Andrei: update width
         fig.update_layout(
             title="Historical number of Hot Days & Nights (M1) Over the Years",
             title_font=dict(size=20),
+            width=1200,
         )
-
-        # update with
-        fig.update_layout(width=1200)
         if self.show_historical_data is True:
             return fig
 
